ff715c0e8fdb555f1f9d2b0b6f28fc127be0fcea
Chanyong Park <chanyong0117@gmail.com>
2024-04-14 22:31:10 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const mynum = 1234;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Get Inputs
+        </div>
+
+        <div>
+          Rendered
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
